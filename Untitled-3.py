6134a189a30b9a1d12fd6d5c9c648f49acb142a8
Putranugroho14 <putranugroho61@gmail.com>
2024-10-15 11:46:40 +0700

    Prak 2

---
+++ Untitled-3.py
@@ -0,0 +1,35 @@
+
+import turtle
+turtle.color('green')
+turtle.forward(100)
+turtle.right(45)
+turtle.color('blue')
+turtle.forward(50)
+turtle.right(45)
+turtle.color('pink')
+turtle.forward(100)
+
+import turtle
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+
+import turtle
+for steps in range(4):
+    turtle.forward(100)
+    turtle.right(90)
+import turtle
+nbrSides = 18
+for steps in range (nbrSides):
+    turtle.forward(100)
+    turtle.right(360/nbrSides)
+    for moresteps in range (nbrSides):
+      turtle.forward(50)
+      turtle.right(360/nbrSides)
+
+
